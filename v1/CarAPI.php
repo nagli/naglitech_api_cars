8db1bced5bce1fe1e75fda57ee034715ef46ebb0
Andreas Nagel <nagli@naglitech.com>
2019-03-06 12:36:44 -0500

    Save one car to the database using POST

--- v1/CarAPI.php
+++ v1/CarAPI.php
@@ -31,18 +31,35 @@ class CarAPI extends API {
                 }
                 break;
             case "DELETE":
-                    if (is_null($this->id)) {
+                if (is_null($this->id)) {
+                    return $this->returnJSON(array(), "cars", array()
+                        , $this->httpStatusCode(400, $this->errorMsg));    
+                } else {
+                    if ($this->deleteOneCar($this->id)) {
                         return $this->returnJSON(array(), "cars", array()
-                            , $this->httpStatusCode(400, $this->errorMsg));    
+                        , $this->httpStatusCode(204));
                     } else {
-                        if ($this->deleteOneCar($this->id)) {
+                        return $this->returnJSON(array(), "cars", array()
+                        , $this->httpStatusCode(304, $this->errorMsg));
+                    }
+                }
+                break;
+            case "POST":
+                if (!is_null($this->arrayInput) AND is_array($this->arrayInput)) {
+                    if (empty($this->verb)) {
+                        /* save one car */
+                        if ($this->saveOneCar($this->arrayInput)) {
                             return $this->returnJSON(array(), "cars", array()
-                            , $this->httpStatusCode(204));
+                        , $this->httpStatusCode(204));
                         } else {
                             return $this->returnJSON(array(), "cars", array()
-                            , $this->httpStatusCode(304, $this->errorMsg));
+                        , $this->httpStatusCode(304, $this->errorMsg));
                         }
                     }
+                } else {
+                    return $this->returnJSON(array(), "cars", array()
+                        , $this->httpStatusCode(400, "No Data"));    
+                }
                 break;
             default:
                 return $this->returnJSON(array(), "cars", array()
@@ -102,6 +119,44 @@ class CarAPI extends API {
             $this->errorMsg = $ex->getMessage();
             return false;    
         }    
+    }
+    private function saveOneCar($data)
+    {
+        if (($cleanData = $this->checkSuppliedData($data, array("make", "model", "platform"))) === false) {
+            return false;
+        }
+        if(($db = $this->connectToDB()) === false) { return false; }
+        $strSQL = "INSERT INTO cars_test 
+        (Make, Model, Platform)
+        VALUE(?,?,?)";
+        try {
+            $stmt = $db->prepare($strSQL);
+            $stmt->bindValue(1, $cleanData['make'], PDO::PARAM_STR);
+            $stmt->bindValue(2, $cleanData['model'], PDO::PARAM_STR);
+            $stmt->bindValue(3, $cleanData['platform'], PDO::PARAM_STR);
+            $stmt->execute();
+            if ($stmt->rowCount()>0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(PDOException $ex) {
+            printf("<br>Error %s: %s", __METHOD__, $ex->getMessage());
+            $this->errorMsg = $ex->getMessage();
+            return false;     
+        }
+    }
+    
+    private function checkSuppliedData(array $data, array $fields)
+    {
+        $cleanData = array();
+        foreach ($data as $key => $value) {
+            if (!in_array(strtolower($key), $fields)) { return false; }
+            $val = filter_var($value, FILTER_SANITIZE_STRING);
+            $val = filter_var($val, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $cleanData[$key] = $val;    
+        }
+        return $cleanData;
     }
 }/* end of class */  
 ?>
