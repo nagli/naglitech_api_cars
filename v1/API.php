2867119d49bf32c0f837717864edb6abf0d9fe47
Andreas Nagel <nagli@naglitech.com>
2019-03-06 11:13:13 -0500

    Get All Cars, Get one Car, and DELETE one car

--- v1/API.php
+++ v1/API.php
@@ -12,11 +12,13 @@ class API {
     protected $args = null;
     protected $endpoint = '';
     protected $verb = '';
-    protected $id = '';
+    protected $id = null;
     protected $addArgs = null;
     protected $method = '';
     protected $arrayInput = null;
     protected $request = null;
+    protected $errorMsg = "";
+    
     public function __construct() 
     {
         //printf("<br>Request:<pre>%s</pre>", var_export($request, true));
@@ -72,12 +74,6 @@ class API {
                 }
             }
         }
-        
-        
-        
-        
-        
-        
          /* Print out for testing */
         printf("<br>Method: %s", var_export($this->method, true));
         printf("<br>Endpoint: %s", var_export($this->endpoint, true));
@@ -86,6 +82,108 @@ class API {
         printf("<br>Args:<pre>%s</pre>", var_export($this->args, true));
         printf("<br>Add Args:<pre>%s</pre>", var_export($this->addArgs, true));
         printf("<br>Input:<pre>%s</pre>", var_export($this->arrayInput, true));
+        
+        if ($this->endpoint === "cars") { 
+            echo $this->cars(); 
+        } else { 
+            throw new Exception("Invalid Endpoint"); 
+        }
+    }
+    protected function calcOffset(array $offset)
+    {
+        if (!empty($this->addArgs) AND isset($this->addArgs['offset']) 
+            AND isset($this->addArgs['limit'])) {
+            if (!is_null($this->addArgs['offset']) 
+                AND $this->addArgs['offset'] != "" 
+                AND is_numeric($this->addArgs['offset'])) {
+                $offset['offset'] = intVal($this->addArgs['offset']);
+            }
+            if (!is_null($this->addArgs['limit']) 
+                AND $this->addArgs['limit'] != "" 
+                AND is_numeric($this->addArgs['limit'])) {
+                $offset['limit'] = intVal($this->addArgs['limit']);
+            }
+            if (($offset['prevOffset'] = 
+                ($offset['offset'] - $offset['limit']))<0) { 
+                    $offset['prevOffset'] = 0; 
+            };
+            $offset['nextOffset'] = ($offset['offset'] + $offset['limit']);
+        }
+
+        return $offset;
+    }
+    protected function connectToDB()
+    {
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=bigblackbear;charset=utf8mb4', 'nagli', 'arn2302');
+        } catch(PDOException $ex) {
+            $this->errorMsg = $ex->getMessage();
+            return false;         
+        }
+        return $db;
+    }
+    protected function httpStatusCode($status, $addInfo=null) 
+    {
+        /**
+        * {
+        *   "status":400,
+        *   "code": "Invalid_address",
+        *   "message": "The origin address is invalid",
+        *   "additionalInformation":""
+        * }
+        */
+        $arrCodes = array(
+            '200' => "OK",
+            '201' => "Created",
+            '204' => "OK",
+            '301' => "Moved_Permanently",
+            '304' => "Not_Modified",
+            '400' => "Bad_Request",
+            '401' => "Unauthorized",
+            '403' => "Forbidden",
+            '404' => "Not_Found",
+            '500' => "Internal_Server_Error"
+        );
+        $arrCodeMessages = array(
+            '200' => "Success",
+            '201' => "Success. The request has been fulfilled and resulted in a new resource being created.",
+            '204' => "Success. The server has fulfilled the request but does not need to return a response body.",
+            '301' => "Moved_Permanently. The request does not exist anymore on this API and is most likly moved to another location.",
+            '304' => "Not_Modified. Nothing was modified, if the purpose of the request was to modify records, no record was modified to any reason.",
+            '400' => "Bad request. The request could not be understood by the server due to malformed syntax.",
+            '401' => "Unauthorized. The request requires authentication with a valid client authorization token.",
+            '403' => "Forbidden. The request has no rights to this API",
+            '404' => "Not found. The server has not found anything matching the Request-URI.",
+            '500' => "Internal server error. The server encountered an unexpected condition which prevented it from fulfilling the request."
+        );
+        $code = "N/A";
+        $message = "N/A";
+        if (empty($addInfo)) { $addInfo = "N/A"; }
+        if (empty($status) OR !key_exists($status, $arrCodes)) { 
+            $status = 500;
+            $addInfo = "No status generated or status is not valid. Error!";
+        }
+
+        $code = $arrCodes[$status];
+        if (isset($arrCodeMessages[$status])) { 
+            $message = $arrCodeMessages[$status];
+        }
+
+        return array(
+            "status" => $status, 
+            "code" => $code, 
+            "message" => $message, 
+            "additionalInformation" => $addInfo);
+    }
+    protected function returnJSON(array $content, $contentKey="content"
+        , array $meta, array $status)
+    {
+        $arr = array();
+        $arr['status'] = $status;
+        $arr['meta'] = $meta;
+        $arr[$contentKey] = $content;
+        $json = json_encode($arr);
+        return $json;
     }
     private function cleanInputs($data) 
     {
--- v1/CarAPI.php
+++ v1/CarAPI.php
@@ -7,5 +7,101 @@ class CarAPI extends API {
     public function __construct($origin) {
         parent::__construct();
     }
+    protected function cars()
+    {
+        switch (strtoupper($this->method)) {
+            case "GET":
+                if (is_null($this->id)) {
+                    /*Get all cars*/
+                    if (($carList=$this->listAllCars())===false) {
+                        return $this->returnJSON(array(), "cars", array()
+                            , $this->httpStatusCode(500, $this->errorMsg));
+                    } else {
+                        return $this->returnJSON($carList, "cars", array()
+                        , $this->httpStatusCode(200));
+                    }
+                } else {
+                    if (($carList=$this->getOneCar($this->id))===false) {
+                        return $this->returnJSON(array(), "cars", array()
+                            , $this->httpStatusCode(500, $this->errorMsg));
+                    } else {
+                        return $this->returnJSON($carList, "cars", array()
+                        , $this->httpStatusCode(200));
+                    }    
+                }
+                break;
+            case "DELETE":
+                    if (is_null($this->id)) {
+                        return $this->returnJSON(array(), "cars", array()
+                            , $this->httpStatusCode(400, $this->errorMsg));    
+                    } else {
+                        if ($this->deleteOneCar($this->id)) {
+                            return $this->returnJSON(array(), "cars", array()
+                            , $this->httpStatusCode(204));
+                        } else {
+                            return $this->returnJSON(array(), "cars", array()
+                            , $this->httpStatusCode(304, $this->errorMsg));
+                        }
+                    }
+                break;
+            default:
+                return $this->returnJSON(array(), "cars", array()
+                        , $this->httpStatusCode(404, "Unknown Method"));     
+        }          
+    }
+    private function listAllCars()
+    {
+        if(($db = $this->connectToDB()) === false) { return false; }
+        $strSQL = "SELECT id, Make, Model, Platform 
+            FROM cars_test
+            ORDER BY Make ASC";
+        try{
+            $stmt = $db->prepare($strSQL);
+            $stmt->execute();
+            return $stmt->fetchall(PDO::FETCH_ASSOC);
+        } catch(PDOException $ex) {
+            printf("<br>Error %s: %s", __METHOD__, $ex->getMessage());
+            $this->errorMsg = $ex->getMessage();
+            return false;    
+        }
+    }
+    private function getOneCar($id)
+    {
+        if(($db = $this->connectToDB()) === false) { return false; }
+        $strSQL = "SELECT id, Make, Model, Platform 
+            FROM cars_test
+            WHERE id = ?";
+        try{
+            $stmt = $db->prepare($strSQL);
+            $stmt->bindValue(1, $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetchall(PDO::FETCH_ASSOC);
+        } catch(PDOException $ex) {
+            printf("<br>Error %s: %s", __METHOD__, $ex->getMessage());
+            $this->errorMsg = $ex->getMessage();
+            return false;    
+        }
+    }
+    private function deleteOneCar($id)
+    {
+        if(($db = $this->connectToDB()) === false) { return false; }
+        $strSQL = "DELETE  
+            FROM cars_test
+            WHERE id = ?";
+        try{
+            $stmt = $db->prepare($strSQL);
+            $stmt->bindValue(1, $id, PDO::PARAM_INT);
+            $stmt->execute();
+            if ($stmt->rowCount()>0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(PDOException $ex) {
+            printf("<br>Error %s: %s", __METHOD__, $ex->getMessage());
+            $this->errorMsg = $ex->getMessage();
+            return false;    
+        }    
+    }
 }/* end of class */  
 ?>
